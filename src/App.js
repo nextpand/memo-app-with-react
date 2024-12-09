38d95cb111a30743a4574ff67a71d9cf72498392
YUURI Inoue <nextpand17@gmail.com>
2024-12-10 00:20:26 +0900

    Page1の追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import { Header } from './components/header';
 import './App.css';
 
 function App() {
   return (
-    <div>あいうえお</div>
+    <div>
+      <Header/>
+    </div>
   );
 }
 
---
+++ src/pages/Page1.js
@@ -0,0 +1,8 @@
+
+export const Page1() => {
+    return (
+        <div>
+            a
+        </div>
+    )
+}
