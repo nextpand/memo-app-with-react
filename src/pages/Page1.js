965b91e12f7ea9ca5bba9a03f8a56e23eb192489
YUURI Inoue <nextpand17@gmail.com>
2024-12-13 16:52:23 +0900

    Page1の文章の追加

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -4,6 +4,8 @@ export const Page1 = () => {
     return (
         <div>
             <Header title="Page1"></Header>
+            <h2>HTMLについて</h2>
+            <p>cssと合わせて書く。</p>
         </div>
     )
 }
